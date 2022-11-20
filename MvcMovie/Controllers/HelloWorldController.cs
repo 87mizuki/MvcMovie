6a8d72e009197b87449b123a28384cd4c2be2980
hana3 <hana3@DESKTOP-QP9U5MO>
2022-11-20 17:19:22 +0900

    プロジェクト ファイルを追加します。

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // /HelloWorldにアクセスしたときの処理
+        public string Index()
+        {
+            return "Hello World";
+        }
+        // /HelloWorld/Welcomeにアクセスしたときの処理
+        public string Welcome(string name, int Id = 1)
+        {
+            return $"名前：{name}さん、ID：{Id}";
+        }
+        //public IActionResult Index()
+        //{
+        //  return View();
+        //}
+    }
+}
